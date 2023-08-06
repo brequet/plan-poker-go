d70ab29ba63f08f8ed688a79ddb8e26090e7255b
brequet <requet.baptiste@gmail.com>
2023-08-06 19:22:17 +0200

    fix '$' char in href

--- front/src/routes/room/[room]/RoomNotFound.svelte
+++ front/src/routes/room/[room]/RoomNotFound.svelte
@@ -7,7 +7,7 @@
 		<h2 class="text-2xl font-bold mb-4">Room Not Found</h2>
 		<p class="text-gray-700">This room does not exist anymore or may have been deleted.</p>
         <p class="text-gray-700">
-        <a href="/room/${roomCode}" class="text-blue-500 no-underline hover:underline">Reload this page</a>
+        <a href="/room/{roomCode}" class="text-blue-500 no-underline hover:underline">Reload this page</a>
          or go back to the <a href="/" class="text-blue-500 no-underline hover:underline">Home page</a>.
         </p>
 	</div>
